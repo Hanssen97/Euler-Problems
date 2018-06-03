442a8c47be7270c5569c1a0e7b5f698f36887185
Jørgen Hanssen <jorkiz97@gmail.com>
2018-06-04 00:11:11 +0200

    Added faster solution (Solution2) to problem 1
    + small changes

--- 1/main.cpp
+++ 1/main.cpp
@@ -1,12 +1,32 @@
 #include <iostream>
 
-int main() {
+
+const size_t CAP = 8000000000;
+
+
+size_t solution1() {
   size_t sum = 0;
 
-  for (size_t number = 0; number < 1000; ++number) {
+  for (size_t number = 0; number < CAP; ++number) {
     if (number % 3 == 0 || number % 5 == 0) sum += number;
   }
 
-  std::cout << "\nsum: " << sum << "\n\n";
+  return sum;
+}
+
+// faster solution
+size_t solution2() {
+  size_t sum = 0, number = 3;
+
+  for (; number < CAP; number += 3)   sum += number;
+  number = 5;
+  for (; number < CAP; number += 5)   if (number % 3 != 0)  sum += number;
+
+  return sum;
+}
+
+
+int main() {
+  std::cout << "\nsum: " << solution2() << "\n\n";
   return 0;
 }
