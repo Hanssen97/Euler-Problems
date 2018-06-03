81690d54810dc20712921c42b7d5049b39559fa3
Jørgen Hanssen <jorkiz97@gmail.com>
2018-06-03 20:16:58 +0200

    Solved another random math problem

---
+++ 2/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+const size_t CAP = 4000000;
+
+
+size_t fibonacci(size_t sum = 2, size_t prev = 1, size_t current = 2) {
+  size_t next = prev + current;
+
+  if (next > CAP)     return sum;
+  if (next % 2 == 0)  sum += next;
+
+  return fibonacci(sum, current, next);
+}
+
+int main() {
+  std::cout << "\nsum: " << fibonacci() << "\n\n";
+  return 0;
+}
